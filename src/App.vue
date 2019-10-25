a85b40dab1113dd8e561c97a8c00647bf4e8e20b
Sosuke Guo <sosuke119@gmail.com>
2019-10-25 15:23:38 +0800

    add ckeditor

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="content">
+      <ckeditor :editor="editor" @ready="onReady" v-model="editorData" :config="editorConfig"></ckeditor>
+      <button @click="submit">submit</button>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
 
+import DecoupledDoc from '@ckeditor/ckeditor5-build-decoupled-document' // ckeditor - 文字編輯器
+import '@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn' // ckeditor - 文字編輯器 - 語言
+const upload = require('@/utils/ckeditorUpload') // ckeditor - 文字編輯器 - upload設定
+export default {
+  data () {
+    return {
+      editor: DecoupledDoc,
+      editorData: '<h1>文档型富文本编辑器</h1>',
+      editorConfig: {
+        // 编辑器的配置
+        language: 'zh-cn',
+        extraPlugins: [upload.MyCustomUploadAdapterPlugin]
+      }
+    }
+  },
+  methods: {
+    // 編輯器 Ready初始化
+    onReady (editor) {
+      // 在可编辑区域之前插入工具栏。
+      editor.ui.getEditableElement().parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      )
+    },
+    // 送出內容
+    submit () {
+      console.log('ckeditor content', this.editorData)
+    }
+  }
+}
+</script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+body
+  margin 50px
+  background gray
+
+.content
+  background white
+  height 500px
+
+.ck-content
+  height 460px
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ckeditor from '@ckeditor/ckeditor5-vue'
+Vue.use(ckeditor)
+
 Vue.config.productionTip = false
 
 new Vue({
